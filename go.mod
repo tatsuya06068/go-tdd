9ef8ec156f623954a6cd4063ba1c925137b5cd7c
tatsuya06068 <fhkt863@gmail.com>
2023-04-02 08:30:56 +0900

    go get install

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.20
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- src/usecase/interator/task.go
+++ src/usecase/interator/task.go
@@ -1,5 +1,30 @@
 package interator
 
+import (
+	"fmt"
+
+	"github.com/tatsuya06068/go-tdd/src/domain/entity"
+	"github.com/tatsuya06068/go-tdd/src/domain/repository"
+	"github.com/tatsuya06068/go-tdd/src/usecase/port"
+)
+
 type TaskInteractor struct {
-	TaskOutPutPort
+	TaskOutPutPort port.TaskOutPutPort
+	TaskRepository repository.TaskRepository
+}
+
+func NewTaskInteractor(taskOutput port.TaskOutPutPort, taskRepository repository.TaskRepository) *TaskInteractor {
+	return &TaskInteractor{
+		TaskOutPutPort: taskOutput,
+		TaskRepository: taskRepository,
+	}
+}
+
+func (i *TaskInteractor) GetTaskData() (*entity.Task, error) {
+	res, err := i.TaskRepository.GetTask()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(res)
+	return res, nil
 }
